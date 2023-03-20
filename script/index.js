6d4475b2ad35915fb57eccf11bfd3e72ca85c94e
Vanina <vani_93_00@hotmail.com>
2023-03-20 14:53:56 -0300

    vue

--- script/index.js
+++ script/index.js
@@ -1,96 +1,177 @@
-let contenedorCards = document.querySelector('#tarjeta');
-// console.log(tarjeta);
-contenedorCards.innerHTML= index(data.events);
+const { createApp } = Vue
+createApp( {
+    data() {
+        return {
+            cardAmazingEvents: null,
+            checkboxes: null,
+            valueBuscador: "",
+            checked: [],
+            cardsFiltradas: undefined
+        }
+    },
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/amazing")
+        .then(Response => Response.json() )
+        .then(data => {
+            if(document.title ==  "upcoming events" ){
+                this.cardAmazingEvents = data.events.filter(evento => evento.date > data.currentDate)
+                console.log(this.cardAmazingEvents)
+            }else if(document.title ==  "past events" ){this.cardAmazingEvents = data.events.filter(evento => evento.date < data.currentDate)
+                
+            }else {this.cardAmazingEvents = data.events}
+            this.cardsFiltradas = this.cardAmazingEvents
+            this.checkboxes = [ ... new Set( this.cardAmazingEvents.map(cardAmazingEvents => cardAmazingEvents.category ) ) ]
+            console.log(this.cardsFiltradas)
+            console.log(this.checkboxes)
+        })
+        .catch(err => console.log(err))
+    },
+    methods:{
+        filtroCruzado: function(){
+            let filtroPorBuscador = this.cardAmazingEvents.filter(cardAmazingEvents => cardAmazingEvents.name.toLowerCase().includes( this.valueBuscador.toLowerCase() ) )
+            let filtroPorChecks = filtroPorBuscador.filter(cardAmazingEvents => this.checked.includes(cardAmazingEvents.category) || this.checked.length === 0 )
+            this.cardsFiltradas = filtroPorChecks
+        }
+    } 
+} ).mount("#app")
 
-function index(arrayData) {
-let cards = '';
 
-for (const evento of arrayData){
-    cards +=  ` 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let contenedorCards = document.querySelector('#tarjeta');
+// // console.log(tarjeta);
+// // contenedorCards.innerHTML= index(data.events);
+// let eventos = []
+
+// function traerDatos(){
+//   fetch('https://mindhub-xj03.onrender.com/api/amazing') 
+// //  fetch('./dat.json')
+// .then(response => response.json())
+// .then(datosApi => {
+// //  console.log(datosApi)
+// eventos = datosApi.events
+// index(events)
+// })
+// .catch(error => console.log(error.message))
+// }
+
+// traerDatos()
+
+// function index(arrayData) {
+// let cards = '';
+
+// for (const evento of arrayData){
+//     cards +=  ` 
    
-    <div class="card m-1 p-2 card-i">
+//     <div class="card m-1 p-2 card-i">
      
-      <img src="${evento.image}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${evento.name}</h5>
-        <p class="card-text">${evento.description}</p>
-      </div>
-      <div class="card-footer">
-          <small class="text-muted">Precio $${evento.price}</small>
+//       <img src="${evento.image}" class="card-img-top" alt="...">
+//       <div class="card-body">
+//         <h5 class="card-title">${evento.name}</h5>
+//         <p class="card-text">${evento.description}</p>
+//       </div>
+//       <div class="card-footer">
+//           <small class="text-muted">Precio $${evento.price}</small>
            
-            <button onclick="seeDetails('${evento._id}')" class="btn btn-secondary m-4" type="button">
-                 ver más
-            </button>
-         </div>
-    </div>
-      `
-}
-return cards;
-}
+//             <button onclick="seeDetails('${evento._id}')" class="btn btn-secondary m-4" type="button">
+//                  ver más
+//             </button>
+//          </div>
+//     </div>
+//       `
+// }
+// return cards;
+// }
 
-function seeDetails(id){
-  // console.log(id);
-  window.location.href= `./vermas.html?id=${id}`;
-}
+// function seeDetails(id){
+//   // console.log(id);
+//   window.location.href= `./vermas.html?id=${id}`;
+// }
 
 
  
 
-let formulario = document.getElementById('formulario');
+// let formulario = document.getElementById('formulario');
 
-// console.log(formulario);
+// // console.log(formulario);
 
-formulario.addEventListener('input',() => {
-
-
-  searchedInfo = formulario.value.toLowerCase();
-  // console.log(searchedInfo);
-  let filteredCards = data.events.filter((eventName) =>
-   eventName.name.toLowerCase().includes(searchedInfo)|| 
-  eventName.description.toLowerCase().includes(searchedInfo))
-  console.log(filteredCards);
-
-  let newCards = index(filteredCards);
-
-  contenedorCards.innerHTML = newCards
-
-});
+// formulario.addEventListener('input',() => {
 
 
-let listadoCategorias = []
-for(let event of data.events){
-    if (!listadoCategorias.includes(event.category)){
-        listadoCategorias.push(event.category)
-    }
-}
-let indexCategorias = ""
-for (let categoria of listadoCategorias){
-    indexCategorias += `<input class="form-check-input" type="checkbox" id="input-${categoria}" value="${categoria}" checked>
-    <label class="form-check-label m-1 mx-2" for="input-${categoria}">${categoria}</label>`
-}
-document.querySelector("#checkbox").innerHTML= indexCategorias
+//   searchedInfo = formulario.value.toLowerCase();
+//   // console.log(searchedInfo);
+//   let filteredCards = data.events.filter((eventName) =>
+//    eventName.name.toLowerCase().includes(searchedInfo)|| 
+//   eventName.description.toLowerCase().includes(searchedInfo))
+//   console.log(filteredCards);
+
+//   let newCards = index(filteredCards);
+
+//   contenedorCards.innerHTML = newCards
+
+// });
+
+
+// let listadoCategorias = []
+// for(let event of data.events){
+//     if (!listadoCategorias.includes(event.category)){
+//         listadoCategorias.push(event.category)
+//     }
+// }
+// let indexCategorias = ""
+// for (let categoria of listadoCategorias){
+//     indexCategorias += `<input class="form-check-input" type="checkbox" id="input-${categoria}" value="${categoria}" checked>
+//     <label class="form-check-label m-1 mx-2" for="input-${categoria}">${categoria}</label>`
+// }
+// document.querySelector("#checkbox").innerHTML= indexCategorias
 
 
 
 
-let check = document.querySelectorAll('input[type=checkbox]')
-check.forEach(input => {
-    input.addEventListener('change', (e)=>{
-      if (e.target.checked) {
-        let categoriaSeleccionada = e.target.value;
-        let eventosFiltrados = data.events.filter(dt => {
-            if (dt.category == categoriaSeleccionada) {
-                console.log(dt.category)
-                return true;
-            }
-        })
-        for(let vnts of eventosFiltrados){
-          console.log(eventosFiltrados);
-           contenedorCards.innerHTML += indexCategorias
-        }
-    }
-})
-})
+// let check = document.querySelectorAll('input[type=checkbox]')
+// check.forEach(input => {
+//     input.addEventListener('change', (e)=>{
+//       if (e.target.checked) {
+//         let categoriaSeleccionada = e.target.value;
+//         let eventosFiltrados = data.events.filter(dt => {
+//             if (dt.category == categoriaSeleccionada) {
+//                 console.log(dt.category)
+//                 return true;
+//             }
+//         })
+//         for(let vnts of eventosFiltrados){
+//           console.log(eventosFiltrados);
+//            contenedorCards.innerHTML += indexCategorias
+//         }
+//     }
+// })
+// })
 
 
 
--- script/vermas.js
+++ script/vermas.js
@@ -1,32 +1,76 @@
-let params = new URLSearchParams(document.location.search)
-let id = params.get("id")
+const { createApp } = Vue
 
-let evento = data.events.filter(evento => evento._id == id);
-console.log(evento);
-const container = document.getElementById("details");
-let cards = "";
+createApp({
+    data(){
+        return {
+            objetos : null,
+            id: undefined,
+            parametros: undefined,
+            idcard: undefined,
+        }
+    }, 
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/amazing")
+        .then(response => response.json())
+        .then(data => {
+            this.objetos = data.events
+            this.parametros = new URLSearchParams(location.search)
+            this.id = this.parametros.get("idUrl")
+            this.idcard = this.objetos.find(objeto => objeto._id == this.id)
+        })
+        .catch(error => console.log(error))
+    },
 
-  cards = `<div class="card mb-3 m-3 d-flex justify-content-center">
-  <div class="row g-0 col-12 ">
-    <div class="col-md-6">
-      <img  id="demas" src="${evento[0].image}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-6">
-      <div class="card-body">
-        <h5 class="card-title">${evento[0].name}</h5>
-        <p class="card-text">${evento[0].description}</p>
-        <p class="card-text">Category: ${evento[0].category}</p>
-        <p class="card-text">Place: ${evento[0].place}</p>
-        <p class="card-text">Capacity: ${evento[0].capacity}</p>
-        <p class="card-text"><small class="text-muted">Precio $${evento[0].price}</small></p>
-      </div>
-    </div>
-  </div>
-</div>
+}).mount('#App')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let params = new URLSearchParams(document.location.search)
+// let id = params.get("id")
+
+// let evento = data.events.filter(evento => evento._id == id);
+// console.log(evento);
+// const container = document.getElementById("details");
+// let cards = "";
+
+//   cards = `<div class="card mb-3 m-3 d-flex justify-content-center">
+//   <div class="row g-0 col-12 ">
+//     <div class="col-md-6">
+//       <img  id="demas" src="${evento[0].image}" class="img-fluid rounded-start" alt="...">
+//     </div>
+//     <div class="col-md-6">
+//       <div class="card-body">
+//         <h5 class="card-title">${evento[0].name}</h5>
+//         <p class="card-text">${evento[0].description}</p>
+//         <p class="card-text">Category: ${evento[0].category}</p>
+//         <p class="card-text">Place: ${evento[0].place}</p>
+//         <p class="card-text">Capacity: ${evento[0].capacity}</p>
+//         <p class="card-text"><small class="text-muted">Precio $${evento[0].price}</small></p>
+//       </div>
+//     </div>
+//   </div>
+// </div>
     
-  `
+//   `
 
-container.innerHTML = cards 
+// container.innerHTML = cards 
 
 
 
